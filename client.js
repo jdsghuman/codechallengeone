5ff576e6daeb477e21d7f57d1d0c9ab6211d8713
JD Ghuman <jdsghuman@gmail.com>
2018-11-12 14:06:12 -0600

    Add swap and delete functionality

--- client.js
+++ client.js
@@ -1,22 +1,38 @@
 $('document').ready(readyNow);
 
-function readyNow(){
-  $('#btn__create').on('click', addDiv);
+function readyNow() {
+  addDiv();
+  swapColors();
+  deleteDiv();
 }
 
-// Click counter
+// Instatiate counter
 let counter = 0;
 
-function addDiv(){
-  // Update counter
-  counter++;
-  let newDiv = $(
-  `<div class="div__new div--red">
-    <p>${counter}</p> 
-    <button>Swap</button>
-    <button>Delete</button>          
-  </div>`);
-  $('#container__div').append(newDiv);
+function addDiv() {
+  $('#btn__create').on('click', function () {
+    // Update counter
+    counter++;
+    // Add new div
+    let newDiv = $(
+      `<div class="div__new div--red">
+        <p>${counter}</p> 
+        <button class="btn__swap">Swap</button>
+        <button class="btn__delete">Delete</button>          
+      </div>`);
+    $('#container__div').append(newDiv);
+  });
+}
 
+function deleteDiv(){
+  $('#container__div').on('click', '.btn__delete', function(){
+    $(this).parent().remove();
+  });
+}
 
+function swapColors() {
+  $('#container__div').on('click', '.btn__swap', function () {
+    $(this).parent().toggleClass('div--red');
+    $(this).parent().toggleClass('div--yellow');
+  });
 }
